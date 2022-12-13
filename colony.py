4ccddb1c11e14d5fe0129eec8a2e77a113bad500
Tarku <v.susini@laposte.net>
2022-12-13 17:04:47 +0100

    Added AddToInventory and RemoveFromInventory function

--- colony.py
+++ colony.py
@@ -2,6 +2,7 @@
 
 from material import *
 from faction import *
+from itempair import *
 
 all_colonies = []
 
@@ -30,7 +31,46 @@ class Colony:
 
     def GiveHeadstart(self):
         for material in mat_all:
-            self.inventory[material] = HEADSTART_QUANTITY
+            self.AddToInventory(
+                ItemPair(
+                    material,
+                    HEADSTART_QUANTITY
+                )
+            )
+
+    def UpdateInventory(self):
+        for (item, count) in self.inventory.items():
+            if count <= 0:
+                self.inventory.pop(item)
+
+    def AddToInventory(self, itemPair: ItemPair):
+        item = itemPair.item
+        count = itemPair.count
+
+        if item not in self.inventory:
+            self.inventory[item] = count
+        else:
+            self.inventory[item] += count
+
+    def RemoveFromInventory(self, itemPair: ItemPair):
+        item = itemPair.item
+        count = itemPair.count
+
+        if item not in self.inventory:
+            print(
+                f"Can't remove {item.name} from inventory. Cause: Item is not in Inventory."
+            )
+            return
+
+        if count > self.inventory[item]:
+            print(
+                f"Can't remove {item.name} x{count} from inventory. Cause: Item is not in sufficient quantity."
+            )
+            return
+
+        self.inventory[item] -= count
+        self.UpdateInventory()
+
 
     def AddMember(self, member: Person):
         self.members.append(member)
